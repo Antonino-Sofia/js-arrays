d75f19f261f5444c31429ece008ad7726c0bfb26
Antonino-Sofia <antoninosofia420@gmail.com>
2025-04-03 15:57:21 +0200

    js-arrays//2

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ console.log(teachers);
 console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-
+console.log(teachers[4]);
+teachers[4] = "Patrick";
+console.log(teachers);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
 
