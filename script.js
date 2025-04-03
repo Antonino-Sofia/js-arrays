e9d3655ca95fc3aa9659155ecbd3d3e89777cfdb
Antonino-Sofia <antoninosofia420@gmail.com>
2025-04-03 16:20:07 +0200

    js-arrays//5

--- script.js
+++ script.js
@@ -16,10 +16,13 @@ console.log(lastTeacher);
 console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(firstTeacher);
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-
+teachers.push("Vanessa");
+console.log(teachers);
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
